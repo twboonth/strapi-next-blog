f7d6e633d129548a180610ff112abbbbcd73f754
TANOz <twboonth@gmail.com>
2024-04-10 19:58:27 +0700

    init repo

---
+++ src/api/spacial-blog/routes/spacial-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * spacial-blog router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::spacial-blog.spacial-blog');
---
+++ src/api/spacial-blog/services/spacial-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * spacial-blog service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::spacial-blog.spacial-blog');
